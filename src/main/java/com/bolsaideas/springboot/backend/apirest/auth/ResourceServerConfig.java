6f487ecfa7f3800c684bca4488d9163e62044884
JhonasPaul <91173212+JhonasPaul@users.noreply.github.com>
2022-05-13 01:28:02 -0500

    security

--- src/main/java/com/bolsaideas/springboot/backend/apirest/auth/ResourceServerConfig.java
+++ src/main/java/com/bolsaideas/springboot/backend/apirest/auth/ResourceServerConfig.java
@@ -22,7 +22,7 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
     /*reglas de seguridad de las rutas, de los metodos Http por el lado de oath*/
 
     @Override/*hasta aca probar en postman*/
-    public void configure(HttpSecurity http) throws Exception {                                     /*cualquiera podra ver las fotos*/
+    public void configure(HttpSecurity http) throws Exception {                             /*cualquiera podra ver las fotos*/      /*/images/**" cualquiera vera la foto por defecto*/
         http.authorizeRequests().antMatchers(HttpMethod.GET,"/api/clientes","/api/clientes/page/**","/api/uploads/img/**", "/images/**").permitAll()
 /*los mismos permisos para clientes/{id} y clietnes/uplaod porque al detalle se accede medientras el id del cliente*/
                 .antMatchers(HttpMethod.GET, "/api/clientes/{id}").hasAnyRole("USER", "ADMIN")
