612ab0d130b005a7d2602e337d31ad5cd7765ebc
JhonasPaul <91173212+JhonasPaul@users.noreply.github.com>
2022-05-14 01:56:33 -0500

    security

--- src/main/java/com/bolsaideas/springboot/backend/apirest/auth/ResourceServerConfig.java
+++ src/main/java/com/bolsaideas/springboot/backend/apirest/auth/ResourceServerConfig.java
@@ -36,7 +36,7 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
     /*importar de import org.springframework.web.cors.CorsConfigurationSource;*/
     public CorsConfigurationSource corsConfigurationSource(){
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+        config.setAllowedOrigins(Arrays.asList("http://localhost:4200"));/*permitir el dominio*/
         config.setAllowedMethods(Arrays.asList("GET","POST","PUT","DELETE","OPTIONS"));
         config.setAllowCredentials(true);
         config.setAllowedHeaders(Arrays.asList("Content-Type", "Authorization"));/*cabeceraS*/
